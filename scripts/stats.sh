60b98c087f3a1be6bbb78035762555a473bd5904
oneearedrabbit <oneearedrabbit@users.noreply.github.com>
2023-04-08 17:56:12 -0400

    Create stats.sh

---
+++ scripts/stats.sh
@@ -0,0 +1,10 @@
+today=$(date -u +"%a, %d %b %Y %T GMT")
+
+for url in $(find /srv/http/example.com/ -name '*.html' | cut -d "/" -f 5-)
+do
+    count=$(rg -e $url -e "$today" /var/log/caddy/example.com-access.log | wc -l)
+    if [ $count != "0" ]
+    then
+        echo "$count - $url"
+    fi
+done | sort -r
