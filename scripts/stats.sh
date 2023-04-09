7db5950ea3adb9bbc8375e521218371472778fe0
oneearedrabbit <oneearedrabbit@users.noreply.github.com>
2023-04-08 20:42:45 -0400

    Update stats.sh

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,8 +1,6 @@
-today=$(date -u +"%a, %d %b %Y %T GMT")
-
 for url in $(find /srv/http/example.com/ -name '*.html' | cut -d "/" -f 5-)
 do
-    count=$(rg -e $url -e "$today" /var/log/caddy/example.com-access.log | wc -l)
+    count=$(rg $url /var/log/caddy/example.com-access.log | wc -l)
     if [ $count != "0" ]
     then
         echo "$count - $url"
