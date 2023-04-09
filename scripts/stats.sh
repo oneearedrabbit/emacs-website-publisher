50fb8cf48dfd3e32813f7fa42e96c7b7671ed4d0
oneearedrabbit <oneearedrabbit@users.noreply.github.com>
2023-04-08 23:28:30 -0400

    Update stats.sh

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,4 +1,4 @@
 today_start=$(date +%s -d "today 00:00:00")
 today_end=$(date +%s -d "today 23:59:59")
 
-jq -rs "[\"Visits\", \"URL\", \"Status\"], (. | map(select(.ts > $todat_start and .ts < $today_end)) | group_by(.request.uri,.status) | map({title: .[0].request.uri, count:length, status: .[0].status}) | sort_by(.count) | .[-15:] | reverse | .[] | [.count, .title, .status]) | @tsv" /var/log/caddy/example.com-access.log | visidata
+jq -rs "[\"Time\", \"URL\", \"Status\"], (. | map(select(.ts > $today_start and .ts < $today_end)) | map({time: .ts | strftime(\"%Y-%m-%d %H:%M:%S\"), title: .request.uri, status: .status}) | .[] | [.time, .title, .status]) | @tsv" /var/log/caddy/example.com-access.log
